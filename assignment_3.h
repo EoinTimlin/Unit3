090576d8f6c29882f36dbd57ceb4045d9d928a53
longfordisashitcounty <58178366+longfordisashitcounty@users.noreply.github.com>
2019-11-27 13:33:01 +0000

    Update assignment_3.h

--- assignment_3.h
+++ assignment_3.h
@@ -2,18 +2,18 @@
 #define ASSIGNMENT_3_H_INCLUDED
 
 // Creates a function to add 2 poloynomials.
-addPoly(*Poly A, *Poly B);
+addPoly(Poly *A, Poly *B);
 // Creates a function that multiplies 2 poloynomials.
-multiplyPoly(*Poly A, *Poly B);
+multiplyPoly(Poly *A, Poly *B);
 // Creates a function that subtracts 2 polynomials.
-subtractPoly(*Poly A, *Poly B);
+subtractPoly(Poly *A, Poly *B);
 // Function that normalises a polynomial
-normalPoly(*Poly A);
+normalPoly(Poly *A);
 // Creates a function that deletes a poloynomial.
-void deletePoly(*Poly A);
+void deletePoly(Poly *A);
 // Creates a function that creates a polynomial.
-void createPoly(*Poly A);
+void createPoly(Poly **A,double con,int pow);
 // Creates a function that prints a polynomial.
-void printPoly(*Poly A);
+void printPoly(Poly *A);
 
 #endif // ASSIGNMENT_3_H_INCLUDED
