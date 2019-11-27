53a8bb9bad3dd527e3c76d5af1327bccd26a713b
longfordisashitcounty <58178366+longfordisashitcounty@users.noreply.github.com>
2019-11-27 13:34:19 +0000

    Update functions.c

--- functions.c
+++ functions.c
@@ -9,3 +9,40 @@ typedef struct Polynomials
   double constant;
   struct Polynomials *next;
 } Poly;
+
+// Function add a new polynomial
+void createPoly(Poly **A,double con,int pow)
+{
+  Poly *x, *y;
+  y = *A;
+  if(y == NULL)
+    {
+      x = (Poly*)malloc(sizeof(Poly));
+      x->constant = con;
+      x->power = pow;
+      *A = x;
+      x->next = (Poly*)malloc(sizeof(Poly));
+      x = x->next;
+      x->next= NULL;
+    }
+  else
+    {
+      x->constant = con;
+      x->power = pow;
+      x->next = (Poly*)malloc(sizeof(Poly));
+      x = x->next;
+      x->next= NULL;
+    }
+}
+
+// Function to print polynomial
+void printPoly(Poly *A) 
+{ 
+while(A->next != NULL) 
+    { 
+    printf("%.1lfx^%d", A->constant, A->power); 
+    A = A->next; 
+    if(A->next != NULL) 
+        printf(" + "); 
+    } 
+}
