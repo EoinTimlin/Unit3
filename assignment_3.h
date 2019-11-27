b2b21dd8f469b4945a93bffb582a7ff9bd451a2a
TomMeehan123 <58112746+TomMeehan123@users.noreply.github.com>
2019-11-27 12:39:08 +0000

    Add files via upload

---
+++ assignment_3.h
@@ -0,0 +1,17 @@
+#ifndef ASSIGNMENT_3_H_INCLUDED
+#define ASSIGNMENT_3_H_INCLUDED
+
+// Creates a function to add 2 poloynomials.
+addPoly(*Poly A, *Poly B);
+// Creates a function that multiplies 2 poloynomials.
+multiplyPoly(*Poly A, *Poly B);
+// Creates a function that subtracts 2 polynomials.
+subtractPoly(*Poly A, *Poly B);
+// Creates a function that deletes a poloynomial.
+void deletePoly(*Poly);
+// Creates a function that creates a polynomial.
+void createPoly(*Poly);
+// Creates a function that prints a polynomial.
+void printPoly(*Poly, int n);
+
+#endif // ASSIGNMENT_3_H_INCLUDED
