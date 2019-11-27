e71a3384f71e2786ead1fa9f8d0a91a263116f54
EoinTimlin <58078541+EoinTimlin@users.noreply.github.com>
2019-11-27 12:46:07 +0000

    Update assignment_3.h

--- assignment_3.h
+++ assignment_3.h
@@ -7,11 +7,13 @@ addPoly(*Poly A, *Poly B);
 multiplyPoly(*Poly A, *Poly B);
 // Creates a function that subtracts 2 polynomials.
 subtractPoly(*Poly A, *Poly B);
+// Function that normalises a polynomial
+normalPoly(*Poly A);
 // Creates a function that deletes a poloynomial.
-void deletePoly(*Poly);
+void deletePoly(*Poly A);
 // Creates a function that creates a polynomial.
-void createPoly(*Poly);
+void createPoly(*Poly A);
 // Creates a function that prints a polynomial.
-void printPoly(*Poly, int n);
+void printPoly(*Poly A);
 
 #endif // ASSIGNMENT_3_H_INCLUDED
