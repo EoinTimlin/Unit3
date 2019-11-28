eea61e108235c7f7e7ba3e5e33dd0c88c2f63d0c
Eoin Timlin <18237908@studentmail.ul.ie>
2019-11-28 21:50:18 +0000

    just some tidying

--- main.c
+++ main.c
@@ -37,7 +37,7 @@ int main()
   printf("Expected: 6x^3 + 8x^2 + 4x^1 + 10x^0\n");
   printf("Actual: ");
   printPoly(poly3);
-  printf("\n");
+  printf("\n\n");
 
   // Subtract Polynomials  
   subtractPoly(poly1, poly2, poly4);
@@ -45,14 +45,14 @@ int main()
   printf("Expected: 6x^3 + 2x^2 + 4x^1 + -6x^0");
   printf("Actual: ");
   printPoly(poly4);
-  printf("\n");
+  printf("\n\n");
 
   // Testing delete function
   printf("Deleting Polynomial 3 ....\n");
   deletePoly(&poly3);
   if(poly3 == NULL)
     {
-      printf("Polynomial fully deleted\n");
+      printf("Polynomial fully deleted\n\n");
     }
   else
     {
@@ -63,7 +63,7 @@ int main()
   int highorder;
   highorder = retHigh(&poly2);
   printf("Order of Polynomial 2: \n");
-  printf("Expected: 3 Actual: %d \n",highorder);
+  printf("Expected: 3 Actual: %d \n\n",highorder);
 
   //Testing multiply function
   Poly *poly6 = NULL;
@@ -74,22 +74,25 @@ int main()
   createPoly(&poly6,2,0);
 
   multiplyPoly(poly6,2);
-
+  printf("Expected: 2x^6 + 10x^5 + 7x^3 + 8x^1 + 4\n");
+  printf("Actual: ");
   printPoly(poly6);
-  printf("\n");
+  printf("\n\n");
   
   //Testing Divide function
   Poly *poly7 = NULL;
   createPoly(&poly7,1,6);
   createPoly(&poly7,5,5);
-  createPoly(&poly7,3.5,3);
+  createPoly(&poly7,3.2,3);
   createPoly(&poly7,4,1);
   createPoly(&poly7,2,0);
 
   dividePoly(poly7,2);
-
+  printf("Dividing 1x^6 + 5x^5 + 3.2x^3 + 4x^1 + 2 by 2\n");
+  printf("Expected: 0.5x^6 + 2.5x^5 + 1.6x^3 + 2x^1 + 1\n");
+  printf("Actual:");
   printPoly(poly7);
-  printf("\n");
+  printf("\n\n");
   return 0;
 }
 
