b31cf56cdf8424b5a111f2feea4a70bd05579f63
longfordisashitcounty <58178366+longfordisashitcounty@users.noreply.github.com>
2019-11-28 10:34:58 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
+#include "assignment_3.h"
 
+int main()
+{
+  Poly *poly = NULL;
+  createPoly(&poly,5,2);
+  createPoly(&poly,4,1);
+  createPoly(&poly,2,0);
+
+  printPoly(poly);
+  printf("\n");
+
+  deletePoly(&poly);
+  if(poly == NULL)
+    {
+      printf("Fully Deleted\n");
+    }
+  
+  return 0;
+}
