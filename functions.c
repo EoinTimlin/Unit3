e4b9b1e0f3d5e07f3d0d4136c86328afccb642d6
Joey <joeyecorbett@Gmail.com>
2019-11-28 13:51:13 +0000

    Revert "some comments"
    This reverts commit 58822049912bb69b4bc42ac0efc6f021cb62cafe.

--- functions.c
+++ functions.c
@@ -1,6 +1,5 @@
 // Functions to work with Polynomials
 
-
 #include "assignment_3.h"
 
 // Function add a new polynomial
@@ -8,17 +7,17 @@ void createPoly(Poly **A,double con,int pow)
 {
   Poly *x, *y;
   y = *A;
-  if(y == NULL) // Check if there free memory at location
+  if(y == NULL)
     {
-      x = (Poly*)malloc(sizeof(Poly)); // Create memory for a poly term
-      x->constant = con; // Set the constant
-      x->power = pow; // Set the power
-      *A = x; 
-      x->next = (Poly*)malloc(sizeof(Poly)); 
+      x = (Poly*)malloc(sizeof(Poly));
+      x->constant = con;
+      x->power = pow;
+      *A = x;
+      x->next = (Poly*)malloc(sizeof(Poly));
       x = x->next;
       x->next= NULL;
     }
-  else // If there is not free memory
+  else
     {
       x->constant = con;
       x->power = pow;
@@ -31,12 +30,12 @@ void createPoly(Poly **A,double con,int pow)
 // Function to print polynomial
 void printPoly(Poly *A) 
 { 
-while(A->next != NULL) // While next part in polynomial isn't NULL, loop through polynomial
+while(A->next != NULL) 
     { 
-    printf("%.1lfx^%d", A->constant, A->power); // Print a part of the expression in the form (constant)x^(power)
+    printf("%.1lfx^%d", A->constant, A->power); 
     A = A->next; 
     if(A->next != NULL) 
-        printf(" + "); // Print a + between the parts
+        printf(" + "); 
     } 
 }
 
@@ -46,37 +45,37 @@ void deletePoly(Poly **A)
   Poly* current = *A; 
   Poly* next; 
   
-   while (current != NULL) // While there is used memory in this location
+   while (current != NULL)  
    { 
        next = current->next; 
-       free(current); // Clear memory location
+       free(current); 
        current = next; 
    }
    
    *A = NULL; 
 }
-// Function to add polynomials A and B together
+// Function to add polynomials
 void addPoly(Poly *A, Poly *B, Poly *C)
 {
-  while(A->next && B->next) 
+  while(A->next && B->next)
   {
-    if(A->power > B->power) // If this term in A is higher degree than in B
+    if(A->power > B->power)
     {
-      C->power = A->power; // Set this term on C to the term in A
+      C->power = A->power;
       C->constant = A->constant;
       A = A->next;
     }
 
-    else if(A->power < B->power) // Else if this term in B is higher degree than the term in A
+    else if(A->power < B->power)
     {
-      C->power = B->power; // Set this term in C to the term in B
+      C->power = B->power;
       C->constant = B->constant;
       B = B->next;
     }
-    else // Otherwise, if they are the same degree
+    else
     {
       C->power = A->power;
-      C->constant = A->constant + B->constant; // Add the term's constants together
+      C->constant = A->constant + B->constant;
       A = A->next;
       B = B->next;
     }
@@ -84,26 +83,26 @@ void addPoly(Poly *A, Poly *B, Poly *C)
      C = C->next;
      C->next = NULL;
   }
-  while(A->next || B->next) // While there is a term in A or B
+  while(A->next || B->next)
   {
-    if(A->next) // If there is a term in A
+    if(A->next)
     {
       C->power = A->power;
-      C->constant = A->constant; // Set the next term in C to A
+      C->constant = A->constant;
       A = A->next;
     }
-    if(B->next) // If there is a term in B
+    if(B->next)
     {
       C->power = B->power;
-      C->constant = B->constant; // Set the next term in C to B
+      C->constant = B->constant;
       B = B->next;
     }
-    C->next = (Poly*)malloc(sizeof(C)); 
+    C->next = (Poly*)malloc(sizeof(C));
     C = C->next;
     C->next = NULL;
   }
 }
-// Function to subtract polynomials A and B from each other
+// Function to subtract polynomials
 subtractPoly(Poly *A, Poly *B, Poly *C)
 {
 while(A->next && B->next)
@@ -154,15 +153,15 @@ while(A->next && B->next)
 
 //multiplies a double by the polynomial
 void multiplyPoly(Poly *A, double c){
-  for (int i = A->power;i>-1;i--){
-    A->constant = (A->constant)*c;
+  while(A->next != NULL){
+   A->constant = (A->constant)/c;
     A = A->next;
   }
 }
 
-//divides a polynomial by a double
+//divides a double by the polynomial
 void dividePoly(Poly *A, double c){
-  for (int i = A->power;i>-1;i--){
+  while(A->next != NULL){
     A->constant = (A->constant)/c;
     A = A->next;
   }
@@ -171,13 +170,39 @@ void dividePoly(Poly *A, double c){
 
 // Function to normalise a polynomial
 void normalPoly(Poly *A){
-  
   double c;
   c = A->constant;
-  
-  for (int i = A->power;i>-1;i--){
+  while(A->power != 0){
     A->constant = (A->constant)/c;
     A = A->next;
   }
+}
+
+// Function to return highest order of polynomial
+int retHigh(Poly **A)
+{
+  Poly* current = *A;
+  int max = -32000;
   
+  while(current->next != NULL)
+    {
+      if(current->power > max)
+	{
+	  if(current->constant != 0) // Checks if coefficient is not zero
+	    {
+	      max =  current->power;
+	      current = current->next;
+	    }
+	  else
+	    {
+	      current = current->next;
+	    }
+	}
+      else
+	{
+	  current = current->next;
+	}
+    }
+  
+  return max;
 }
