40276f6bae6998876e4c2a98a510fa869411bf91
EoinTimlin <58078541+EoinTimlin@users.noreply.github.com>
2019-11-28 13:37:07 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 int main()
 {
+  //Creating Polynomials 
   Poly *poly1 = NULL;
   createPoly(&poly1,5,2);
   createPoly(&poly1,4,1);
@@ -9,10 +10,12 @@ int main()
   createPoly(&poly2,6,3);
   createPoly(&poly2,3,2);
   createPoly(&poly2,8,0);
-
+  
+  //Poly for addition
   Poly *poly3 = NULL;
   poly3 = (Poly*)malloc(sizeof(poly3));
 
+  //Poly for Subtraction
   Poly *poly4 = NULL;
   poly4 = (Poly*)malloc(sizeof(poly4));
 
@@ -22,11 +25,13 @@ int main()
   printPoly(poly2);
   printf("\n");
 
+  // Add Polynomials
   addPoly(poly1, poly2, poly3);
 
   printPoly(poly3);
   printf("\n");
 
+  // Subtract Polynomials  
   subtractPoly(poly1, poly2, poly4);
 
   printPoly(poly4);
