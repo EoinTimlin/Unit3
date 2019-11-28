50d82ab439be52fe5ddb8ea02438d94919e22565
EoinTimlin <58078541+EoinTimlin@users.noreply.github.com>
2019-11-28 11:31:03 +0000

    Update functions.c

--- functions.c
+++ functions.c
@@ -152,6 +152,23 @@ while(A->next && B->next)
   }
 }
 
+//multiplies a double by the polynomial
+void multiplyPoly(Poly *A, double c){
+  for (int i = A->power;i>-1;i--){
+    A->constant = (A->constant)*c;
+    A = A->next;
+  }
+}
+
+//multiplies a double by the polynomial
+void dividePoly(Poly *A, double c){
+  for (int i = A->power;i>-1;i--){
+    A->constant = (A->constant)/c;
+    A = A->next;
+  }
+}
+
+
 // Function to normalise a polynomial
 void normalPoly(Poly *A){
   
