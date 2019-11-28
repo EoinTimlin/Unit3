dc0d81bcbed93fe0b9344055829e7866c1da9072
TomMeehan123 <58112746+TomMeehan123@users.noreply.github.com>
2019-11-28 13:49:22 +0000

    Update main.c

--- main.c
+++ main.c
@@ -20,21 +20,29 @@ int main()
   poly4 = (Poly*)malloc(sizeof(poly4));
 
   printPoly(poly1);
+  printf("Expected: 5x^2 + 4x^1 + 2x^0\n");
+  printf("Actual: ");
   printf("\n");
 
   printPoly(poly2);
+  printf("Expected: 6x^3 + 3x^2 + 8x^0\n");
+  printf("Actual: ");
   printf("\n");
 
   // Add Polynomials
   addPoly(poly1, poly2, poly3);
 
   printPoly(poly3);
+  printf("Expected: 6x^3 + 8x^2 + 4x^1 + 10x^0\n");
+  printf("Actual: ");
   printf("\n");
 
   // Subtract Polynomials  
   subtractPoly(poly1, poly2, poly4);
 
   printPoly(poly4);
+  printf("Expected: 6x^3 + 2x^2 + 4x^1 + -6x^0");
+  printf("Actual: ");
   printf("\n");
 
   // Testing delete function
